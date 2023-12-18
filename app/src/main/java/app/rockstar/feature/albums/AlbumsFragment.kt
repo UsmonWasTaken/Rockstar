e6d682e8d3e0a4043a6c0c16e9ce93e1b76c140b
Usmon Abdurakhmanov <usmonwastaken@gmail.com>
2023-12-18 14:31:28 +0500

    add initial features

---
+++ app/src/main/java/app/rockstar/feature/albums/AlbumsFragment.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023 The Rockstar authors and contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package app.rockstar.feature.albums
+
+import androidx.fragment.app.Fragment
+import app.rockstar.android.R
+
+class AlbumsFragment : Fragment(R.layout.fragment_albums)
---
+++ app/src/main/java/app/rockstar/feature/search/SearchFragment.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023 The Rockstar authors and contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+
+package app.rockstar.feature.search
+
+import androidx.fragment.app.Fragment
+import app.rockstar.android.R
+
+class SearchFragment : Fragment(R.layout.fragment_search)
